2a76f3488aa1416b26bcf6730f3e86450eefde76
bab2min <bab2min@gmail.com>
2024-07-31 18:05:43 +0900

    Change to using cmake for install package

--- setup.py
+++ setup.py
@@ -1,9 +1,156 @@
-from setuptools import setup, Extension
-from setuptools.command.install import install
-from codecs import open
-import os, os.path, struct, re, platform
+import os, os.path
+from glob import glob
+import platform
+import sys
+import shutil
+import subprocess
+import re
 
-import numpy
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+from distutils.version import LooseVersion
+from sysconfig import get_platform
+
+import numpy as np
+
+
+def get_extra_cmake_options():
+    """read --clean, --no, --set, --compiler-flags, and -G options from the command line and add them as cmake switches.
+    """
+    _cmake_extra_options = ["-DCMAKE_POSITION_INDEPENDENT_CODE=1"]
+    if os.environ.get('KIWI_CPU_ARCH'):
+        _cmake_extra_options.append("-DKIWI_CPU_ARCH=" + os.environ['KIWI_CPU_ARCH'])
+    if os.environ.get('MACOSX_DEPLOYMENT_TARGET'):
+        _cmake_extra_options.append("-DCMAKE_OSX_DEPLOYMENT_TARGET=" + os.environ['MACOSX_DEPLOYMENT_TARGET'])
+    _clean_build_folder = False
+
+    opt_key = None
+
+    argv = [arg for arg in sys.argv]  # take a copy
+    # parse command line options and consume those we care about
+    for arg in argv:
+        if opt_key == 'compiler-flags':
+            _cmake_extra_options.append('-DCMAKE_CXX_FLAGS={arg}'.format(arg=arg.strip()))
+        elif opt_key == 'G':
+            _cmake_extra_options += ['-G', arg.strip()]
+        elif opt_key == 'no':
+            _cmake_extra_options.append('-D{arg}=no'.format(arg=arg.strip()))
+        elif opt_key == 'set':
+            _cmake_extra_options.append('-D{arg}'.format(arg=arg.strip()))
+
+        if opt_key:
+            sys.argv.remove(arg)
+            opt_key = None
+            continue
+
+        if arg == '--clean':
+            _clean_build_folder = True
+            sys.argv.remove(arg)
+            continue
+
+        if arg == '--yes':
+            print("The --yes options to kiwipiepy's setup.py don't do anything since all these options ")
+            print("are on by default.  So --yes has been removed.  Do not give it to setup.py.")
+            sys.exit(1)
+        if arg in ['--no', '--set', '--compiler-flags']:
+            opt_key = arg[2:].lower()
+            sys.argv.remove(arg)
+            continue
+        if arg in ['-G']:
+            opt_key = arg[1:]
+            sys.argv.remove(arg)
+            continue
+
+    return _cmake_extra_options, _clean_build_folder
+
+def num_available_cpu_cores(ram_per_build_process_in_gb):
+    import multiprocessing
+    try:
+        mem_bytes = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')  
+        mem_gib = mem_bytes/(1024.**3)
+        num_cores = multiprocessing.cpu_count() 
+        mem_cores = int(mem_gib/float(ram_per_build_process_in_gb)+0.5)
+        return max(min(num_cores, mem_cores), 1)
+    except ValueError:
+        return 2 # just assume 2 if we can't get the os to tell us the right answer.
+
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir='', cmake_options=[], *args, **kwargs):
+        Extension.__init__(self, name, sources=[], *args, **kwargs)
+        self.sourcedir = os.path.abspath(sourcedir)
+        self.additional_options = cmake_options
+
+class CMakeBuild(build_ext):
+
+    def get_cmake_version(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except:
+            sys.stderr.write("\nERROR: CMake must be installed to build tomotopy\n\n") 
+            sys.exit(1)
+        return re.search(r'version\s*([\d.]+)', out.decode()).group(1)
+
+    def run(self):
+        cmake_version = self.get_cmake_version()
+        if platform.system() == "Windows":
+            if LooseVersion(cmake_version) < '3.1.0':
+                sys.stderr.write("\nERROR: CMake >= 3.1.0 is required on Windows\n\n")
+                sys.exit(1)
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        libs = self.get_libraries(ext)
+
+        cmake_args = [
+            '-DINCLUDE_DIRS={}'.format(';'.join(self.include_dirs + ext.include_dirs)),
+            '-DLIBRARY_DIRS={}'.format(';'.join(self.library_dirs)),
+            '-DLIBRARIES={}'.format(';'.join(libs)),
+            '-DPYTHON_EXECUTABLE=' + sys.executable,
+        ]
+        cmake_args += ext.additional_options
+        print(cmake_args)
+
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
+
+        if platform.system() == "Windows":
+            if sys.maxsize > 2**32:
+                cmake_args += ['-A', 'x64']
+            else:
+                cmake_args += ['-A', 'Win32']
+            build_args += ['--', '/m'] 
+        else:
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+            build_args += ['--', '-j'+str(num_available_cpu_cores(2))]
+
+        build_folder = os.path.abspath(self.build_temp)
+
+        if not os.path.exists(build_folder):
+            os.makedirs(build_folder)
+
+        cmake_setup = ['cmake', ext.sourcedir] + cmake_args
+        cmake_build = ['cmake', '--build', '.'] + build_args
+
+        print("Building extension for Python {}".format(sys.version.split('\n',1)[0]))
+        print("Invoking CMake setup: '{}'".format(' '.join(cmake_setup)))
+        sys.stdout.flush()
+        subprocess.check_call(cmake_setup, cwd=build_folder)
+        print("Invoking CMake build: '{}'".format(' '.join(cmake_build)))
+        sys.stdout.flush()
+        subprocess.check_call(cmake_build, cwd=build_folder)
+        
+        fullname = self.get_ext_fullname(ext.name)
+        filename = self.get_ext_filename(fullname)
+
+        if platform.system() == "Windows":
+            shutil.move(os.sep.join([build_folder, cfg, f'_tomotopy_target.dll']), os.sep.join([extdir, filename]))
+        else:
+            shutil.move(glob(os.sep.join([build_folder, 'lib_tomotopy_target.*']))[0], os.sep.join([extdir, filename]))
+
 
 try:
     from setuptools._distutils import _msvccompiler
@@ -20,7 +167,15 @@ else:
     except:
         tomotopy_cpu_arch = 'x86_64'
 
-from sysconfig import get_platform
+
+exec(open('tomotopy/_version.py').read())
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+long_description = ''
+for line in open(os.path.join(here, 'tomotopy/documentation.rst'), encoding='utf-8'):
+    long_description += re.sub(r'^<.+>\s*$', '', line)
+
 fd = get_platform().split('-')
 if fd[0] == 'macosx':
     if os.environ.get('MACOSX_DEPLOYMENT_TARGET'): 
@@ -36,73 +191,57 @@ if fd[0] == 'macosx':
         fd[1] = os.environ['MACOSX_DEPLOYMENT_TARGET']
     os.environ['_PYTHON_HOST_PLATFORM'] = '-'.join(fd[:-1] + [tomotopy_cpu_arch])
     
-exec(open('tomotopy/_version.py').read())
+cmake_extra_options, _ = get_extra_cmake_options()
 
-here = os.path.abspath(os.path.dirname(__file__))
+build_for_manyplatform = ('many' in os.environ.get('AUDITWHEEL_PLAT', ''))
 
-long_description = ''
-for line in open(os.path.join(here, 'tomotopy/documentation.rst'), encoding='utf-8'):
-    long_description += re.sub(r'^<.+>\s*$', '', line)
-
-sources = []
-for f in os.listdir(os.path.join(here, 'src/python')):
-    if f.endswith('.cpp') and not f.endswith('py_rt.cpp'): sources.append('src/python/' + f)
-for f in os.listdir(os.path.join(here, 'src/TopicModel')):
-    if f.endswith('.cpp'): sources.append('src/TopicModel/' + f)
-for f in os.listdir(os.path.join(here, 'src/Labeling')):
-    if f.endswith('.cpp'): sources.append('src/Labeling/' + f)
-
-largs = []
-arch_levels = {'':'', 'sse2':'-msse2', 'avx':'-mavx', 'avx2':'-mavx2 -mfma'}
+arch_levels = ['none', 'sse2', 'avx2']
 if platform.system() == 'Windows': 
-    cargs = ['/O2', '/MT', '/Gy', '/D__SSE2__']
-    arch_levels = {'':'', 'sse2':'/arch:SSE2', 'avx':'/arch:AVX', 'avx2':'/arch:AVX2'}
+    arch_levels = ['none', 'sse2', 'avx2']
 elif platform.system() == 'Darwin': 
-    cargs = ['-std=c++1y', '-O3', '-fpermissive', '-stdlib=libc++', '-Wno-unused-variable', '-Wno-switch']
-    largs += ['-stdlib=libc++']
-    if 'many' in os.environ.get('AUDITWHEEL_PLAT', ''): cargs.append('-g0')
     if tomotopy_cpu_arch == 'arm64':
-        arch_levels = {'': ''}
-        cargs += ['-DTOMOTOPY_ISA=arm64', '-arch', 'arm64']
-        largs += ['-arch', 'arm64']
-    else:
-        if 'many' not in os.environ.get('AUDITWHEEL_PLAT', ''): arch_levels = {'':'-msse2'}
-        cargs += ['-arch', 'x86_64']
-        largs += ['-arch', 'x86_64']
-elif 'many' in os.environ.get('AUDITWHEEL_PLAT', ''):
-    cargs = ['-std=c++1y', '-O3', '-fpermissive', '-g0', '-Wno-unused-variable', '-Wno-switch']
+        arch_levels = ['aarch64']
+    elif build_for_manyplatform: 
+        arch_levels = ['sse2']
+elif build_for_manyplatform:
     if tomotopy_cpu_arch in ('arm64', 'aarch64'):
-        arch_levels = {'': ''}
-        cargs += ['-DTOMOTOPY_ISA=arm64']
+        arch_levels = ['aarch64']
 else:
-    cargs = ['-std=c++1y', '-O3', '-fpermissive', '-Wno-unused-variable', '-Wno-switch']
-    arch_levels = {'': '-march=native'}
-
-if struct.calcsize('P') < 8: arch_levels = {k:v for k, v in arch_levels.items() if k in ('', 'sse2')}
-else: arch_levels = {k:v for k, v in arch_levels.items() if k not in ('sse2',)}
-
-lang_macro = []
-if os.environ.get('TOMOTOPY_LANG') == 'kr': lang_macro = [('DOC_KO', '1')]
+    arch_levels = ['native']
 
 modules = []
 if len(arch_levels) > 1:
-    modules.append(Extension('_tomotopy',
+    modules.append(CMakeExtension('_tomotopy',
         libraries=[],
-        include_dirs=['include', numpy.get_include()],
-        sources=['src/python/py_rt.cpp'],
-        extra_compile_args=cargs, extra_link_args=largs))
+        include_dirs=['include', np.get_include()],
+        cmake_options=cmake_extra_options + ['-DEXT_TYPE=dispatcher'],
+    ))
 
-for arch, aopt in arch_levels.items():
+if build_for_manyplatform:
+    cmake_extra_options += ['-DNO_DEBUG_INFO=1']
+else:
+    cmake_extra_options += ['-DNO_DEBUG_INFO=0']
+
+if os.environ.get('TOMOTOPY_LANG'):
+    cmake_extra_options += ['-DTOMOTOPY_LANG=' + os.environ['TOMOTOPY_LANG']]
+
+for arch in arch_levels:
     if len(arch_levels) > 1:
-        module_name = '_tomotopy_' + (arch or 'none')
+        if arch in ('none', 'native', 'aarch64'):
+            module_name = '_tomotopy_none'
+        else:
+            module_name = '_tomotopy_' + arch
     else:
         module_name = '_tomotopy'
-    modules.append(Extension(module_name,
-                    libraries=[],
-                    include_dirs=['include', numpy.get_include()],
-                    sources=sources,
-                    define_macros=[('MODULE_NAME', 'PyInit_' + module_name)] + lang_macro,
-                    extra_compile_args=cargs + (aopt.split(' ') if aopt else []), extra_link_args=largs))
+    modules.append(CMakeExtension(module_name,
+        libraries=[],
+        include_dirs=['include', np.get_include()],
+        cmake_options=cmake_extra_options + [
+            '-DEXT_TYPE=handler', 
+            '-DTARGET_ARCH=' + arch,
+            '-DTOMOTOPY_ISA=' + arch, 
+            '-DMODULE_NAME=PyInit_' + module_name],
+    ))
 
 setup(
     name='tomotopy',
@@ -140,10 +279,11 @@ setup(
         "Operating System :: POSIX",
         "Operating System :: MacOS"
     ],
-    install_requires=['numpy>=1.11.0'],
+    install_requires=['numpy>=1.11.0,<2'],
     keywords='NLP,Topic Model',
 
     packages=['tomotopy'],
     include_package_data=True,
-    ext_modules=modules
+    ext_modules=modules,
+    cmdclass=dict(build_ext=CMakeBuild),
 )
