85b5cc78da0a134e990e79032b544a4fe01849ce
bab2min <bab2min@gmail.com>
2024-08-06 00:32:22 +0900

    Update documentation

--- tomotopy/viewer/__init__.py
+++ tomotopy/viewer/__init__.py
@@ -12,6 +12,12 @@ The following is a simple example of how to use the viewer:
     tp.viewer.open_viewer(mdl, port=9999)
     # open http://localhost:9999 in your web browser
 
+Or you can run the viewer from the command line:
+::
+
+    python -m tomotopy.viewer a_trained_model.bin --host localhost --port 9999
+    # open http://localhost:9999 in your web browser
+    
 For more details, please refer to the `tomotopy.viewer.viewer_server.open_viewer` function.
 '''
 
